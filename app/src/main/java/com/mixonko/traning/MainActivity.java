5b4805206d5a1d9ab3ee77f6c64018520591d816
mixonko <m1zan1rop@mail.ru>
2020-02-09 19:04:51 +0300

    create RecyclerView, NoteAdapter

--- app/src/main/java/com/mixonko/traning/MainActivity.java
+++ app/src/main/java/com/mixonko/traning/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.mixonko.traning.adapter.NoteAdapter;
 import com.mixonko.traning.entity.Note;
 import com.mixonko.traning.myAppContext.MyApplication;
 import com.mixonko.traning.viewModel.NoteViewModel;
@@ -24,11 +28,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MyApplication.getAppContext()));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter noteAdapter = new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                Toast.makeText(MyApplication.getAppContext(), "Has been updated", Toast.LENGTH_LONG).show();
+                noteAdapter.setNotes(notes);
             }
         });
 
